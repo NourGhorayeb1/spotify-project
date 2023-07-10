f06bf4ad76385101cd665376d4df58b1f3373ed7
Nour Ghorayeb <nour.ghorayeb99@gmail.com>
2023-07-10 11:18:24 +0300

    Last Changes

--- src/app/components/register-form/register-form.component.ts
+++ src/app/components/register-form/register-form.component.ts
@@ -1,5 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { createState, PropsFactory, StateOf } from '@ngneat/elf';
+// interface Artist {
+//   firstName: string;
+//   lastName: string;
+//   // Add other fields as needed
+// }
 
 @Component({
   selector: 'app-register-form',
@@ -8,6 +14,7 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 })
 export class RegisterFormComponent implements OnInit {
   registrationForm!: FormGroup;
+  // artistState: StateOf<Artist[]> = createState<Artist[]>([]);
 
   constructor(private formBuilder: FormBuilder) { }
 
@@ -30,7 +37,7 @@ export class RegisterFormComponent implements OnInit {
     if (this.registrationForm.valid) {
       console.log(this.registrationForm.value);
     } else {
-      // Handle form validation errors
+      console.log(this.registrationForm.status) // INVALID
     }
   }
 }
---
+++ src/app/registration.state.ts
@@ -0,0 +1,16 @@
+// import { createEntityState } from '@ngneat/elf';
+
+// export interface ArtistRegistration {
+//   firstName: string;
+//   lastName: string;
+//   dateOfBirth: string;
+//   email: string;
+//   phoneNumber: string;
+//   profilePicture: string;
+//   stageName: string;
+//   backstory: string;
+//   startingDate: string;
+//   albums: string[];
+// }
+
+// export const registrationState = createEntityState<ArtistRegistration>({});
